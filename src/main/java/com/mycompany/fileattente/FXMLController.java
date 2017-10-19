cb636cc494aa08d35fde2d88b96f7f96f253fc94
Benjamin Caldas <benjamincaldas@hotmail.fr>
2017-10-19 21:14:40 +0200

    Ajout des tabs

--- src/main/java/com/mycompany/fileattente/FXMLController.java
+++ src/main/java/com/mycompany/fileattente/FXMLController.java
@@ -8,26 +8,35 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
     
-    @FXML
-    private Label label_libeleChoix;
-    @FXML
-    private RadioButton radioButton_MM1;
-    @FXML
-    private RadioButton radioButton_MM1K;
-    @FXML
-    private RadioButton radioButton_MMS;
-    @FXML
-    private Button bouton_validerType;
-    @FXML
-    private Label label_choix;
+    // 1st tab
+    @FXML private Label label_libeleChoix;
+    @FXML private RadioButton radioButton_MM1;
+    @FXML private RadioButton radioButton_MM1K;
+    @FXML private RadioButton radioButton_MMS;
+    @FXML private Button bouton_validerType;
+    @FXML private Label label_choix;
+    private int choix;
+    // 2nd tab
+    @FXML private Label label_libeleParametres;
+    @FXML private Label label_lambda;
+    @FXML private TextField textField_lambda;
+    @FXML private Label label_mu;
+    @FXML private TextField textField_mu;
+    @FXML private Label label_K;
+    @FXML private TextField textField_K;
+    @FXML private Label label_S;
+    @FXML private TextField textField_S;
+    @FXML private Button bouton_validerParametres;
     
     @FXML
     private void handleChoice1Action(ActionEvent event) {
         if (radioButton_MM1.isSelected()) {
-            label_choix.setText("Choix : 1");
+            choix = 1;
+            label_choix.setText("Choix : " + choix);
             radioButton_MM1K.setSelected(false);
             radioButton_MMS.setSelected(false);
             bouton_validerType.setDisable(false);
@@ -40,7 +49,8 @@ public class FXMLController implements Initializable {
     @FXML
     private void handleChoice2Action(ActionEvent event) {
         if (radioButton_MM1K.isSelected()) {
-            label_choix.setText("Choix : 2");
+            choix = 2;
+            label_choix.setText("Choix : " + choix);
             radioButton_MM1.setSelected(false);
             radioButton_MMS.setSelected(false);
             bouton_validerType.setDisable(false);
@@ -53,7 +63,8 @@ public class FXMLController implements Initializable {
     @FXML
     private void handleChoice3Action(ActionEvent event) {
         if (radioButton_MMS.isSelected()) {
-            label_choix.setText("Choix : 3");
+            choix = 3;
+            label_choix.setText("Choix : " + choix);
             radioButton_MM1.setSelected(false);
             radioButton_MM1K.setSelected(false);
             bouton_validerType.setDisable(false);
@@ -65,8 +76,8 @@ public class FXMLController implements Initializable {
     
     @FXML
     private void handleSubmitAction(ActionEvent event) {
-        System.out.println("Choix : ");
-        label_choix.setText("Choix : ");
+        System.out.println("Choix : " + choix);
+        label_choix.setText("Choix : " + choix);
     }
     
     @Override
